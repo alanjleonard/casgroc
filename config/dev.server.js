5dfaa675836b2f17666f364e1820479ae132700b
AL Leonard <aleonard@sparkgeo.com>
2020-12-30 16:40:24 -0500

    netlify server config

--- config/dev.server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6f4d52e5a16e59038d946b6b62fa3a45'),
-    },
-  },
-});
