d4de895fdae493f828a38af2871ed73f413791f6
AL Leonard <aleonard@sparkgeo.com>
2020-12-30 15:01:21 -0500

    removed dashboard url - server configs

--- config/server.js
+++ config/server.js
@@ -3,7 +3,6 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: 'https://casgroc-api.netlify.app/',
   admin: {
-    url: '/',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6f4d52e5a16e59038d946b6b62fa3a45'),
     },
