97ab4274edcdd726f58209d08182bcd958fc234b
AL Leonard <aleonard@sparkgeo.com>
2020-12-30 16:50:37 -0500

    netlify server config

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: 'https://casgroc-api.netlify.app/',
+  url: 'https://casgroc-api.netlify.app/',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6f4d52e5a16e59038d946b6b62fa3a45'),
